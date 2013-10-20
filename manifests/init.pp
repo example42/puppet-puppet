4a8ac53c7e60c46b94c85c43ffd72ec934a6ba8a
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-21 01:52:12 +0200

    Disabling ipv4 fw rules

--- manifests/init.pp
+++ manifests/init.pp
@@ -837,6 +837,7 @@ class puppet (
       action         => 'allow',
       direction      => 'output',
       enable         => $puppet::manage_firewall,
+      enable_v4      => false,
     }
 
     firewall::rule { "puppet_${puppet::protocol}_${puppet::port}-in":
@@ -847,6 +848,7 @@ class puppet (
       direction                 => 'input',
       iptables_explicit_matches => { 'state' => { 'state' => 'RELATED,ESTABLISHED' } },
       enable                    => $puppet::manage_firewall,
+      enable_v4                 => false,
     }
 
     if $puppet::bool_listen == true {
