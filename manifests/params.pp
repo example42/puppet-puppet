26be85941dd602baa90509c8c9b9d14de58be7f0
Jared Nelson <jared@ajpadilla.com>
2014-03-12 13:17:11 -0500

    fix log_dir permissions for OpenBSD

--- manifests/params.pp
+++ manifests/params.pp
@@ -116,7 +116,13 @@ class puppet::params {
   }
 
   $process_user_server = $::operatingsystem ? {
-    default => 'puppet',
+    /(?i:OpenBSD)/ => '_puppet',
+    default        => 'puppet',
+  }
+
+  $process_group_server = $::operatingsystem ? {
+    /(?i:OpenBSD)/ => '_puppet',
+    default        => 'puppet',
   }
 
   $version_server = 'present'
