56f24f96b5a552f8fcb0862a7a4170fc406b7aef
David Schmitt <david@dasz.at>
2014-10-07 17:01:49 +0200

    tests: add concat_basedir fact to fix firewall tests

--- spec/classes/puppet_spec.rb
+++ spec/classes/puppet_spec.rb
@@ -4,7 +4,10 @@ describe 'puppet' do
 
   let(:title) { 'puppet' }
   let(:node) { 'rspec.example42.com' }
-  let(:facts) { { :ipaddress => '10.42.42.42' } }
+  let(:facts) { {
+    :ipaddress => '10.42.42.42',
+    :concat_basedir => '/tmp/cb',
+  } }
 
   describe 'Test standard installation' do
     it { should contain_package('puppet').with_ensure('present') }
