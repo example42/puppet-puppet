331983084986c05b5516e70a11fcf83f5df125f9
Alessandro Franceschi <al@example42.com>
2016-12-03 09:09:05 +0100

    Better PE presence check

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,8 @@ class puppet (
 
 ) {
 
-  if $::pe_build {
+  # This is the only PE related fact available also on clients
+  if $::pe_concat_basedir {
     notice('This module does not manage PE')
   } else {
 
